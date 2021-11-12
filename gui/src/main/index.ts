6bde92c223eaac4dd18777721baffa1cc6718871
Oskar Nyberg <oskar@mullvad.net>
2021-11-12 13:27:49 +0100

    Improve error handling for daemon events

--- gui/src/main/index.ts
+++ gui/src/main/index.ts
@@ -755,7 +755,6 @@ class ApplicationMain {
       },
       (error: Error) => {
         log.error(`Cannot deserialize the daemon event: ${error.message}`);
-        log.error(error.stack);
       },
     );
 
