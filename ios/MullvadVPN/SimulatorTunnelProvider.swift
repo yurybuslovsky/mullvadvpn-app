6d61b7056be1544eb68820d6a067a36b35649d15
Andrej Mihajlov <and@mullvad.net>
2022-02-24 12:11:07 +0100

    VPNConnectionProtocol: implement connectedDate

--- ios/MullvadVPN/SimulatorTunnelProvider.swift
+++ ios/MullvadVPN/SimulatorTunnelProvider.swift
@@ -13,6 +13,7 @@ import NetworkExtension
 
 protocol VPNConnectionProtocol: NSObject {
     var status: NEVPNStatus { get }
+    var connectedDate: Date? { get }
 
     func startVPNTunnel() throws
     func startVPNTunnel(options: [String: NSObject]?) throws
@@ -163,6 +164,21 @@ class SimulatorVPNConnection: NSObject, VPNConnectionProtocol {
         }
     }
 
+    private var _connectedDate: Date?
+    private(set) var connectedDate: Date? {
+        get {
+            lock.lock()
+            defer { lock.unlock() }
+
+            return _connectedDate
+        }
+        set {
+            lock.lock()
+            _connectedDate = newValue
+            lock.unlock()
+        }
+    }
+
     func startVPNTunnel() throws {
         try startVPNTunnel(options: nil)
     }
@@ -175,8 +191,10 @@ class SimulatorVPNConnection: NSObject, VPNConnectionProtocol {
         SimulatorTunnelProvider.shared.delegate.startTunnel(options: options) { (error) in
             if error == nil {
                 self.status = .connected
+                self.connectedDate = Date()
             } else {
                 self.status = .disconnected
+                self.connectedDate = nil
             }
         }
     }
@@ -186,6 +204,7 @@ class SimulatorVPNConnection: NSObject, VPNConnectionProtocol {
 
         SimulatorTunnelProvider.shared.delegate.stopTunnel(with: .userInitiated) {
             self.status = .disconnected
+            self.connectedDate = nil
         }
     }
 
